10a6f10993c1e6fecf76a90ff9c61b222b8bada2
Everlin Hernandez <everlinhernandez93@gmail.com>
2025-05-18 13:21:28 -0700

    Refactor app.js

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", {
 app.use(cors()); 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    req.user = { _id: '5d8b8592978f8bd833ca8133' };
+    next();
+  });
 
 app.post("/signin", login);
 app.post("/signup", registerUser);
